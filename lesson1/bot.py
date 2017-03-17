ec02341863aec61d00e1b1c28553d1c26c283939
Alexander Lebedev <lebedev@0x4e3.ru>
2017-03-17 22:55:07 +0300

    Adds /planet command to the telegram bot

--- lesson1/bot.py
+++ lesson1/bot.py
@@ -1,6 +1,9 @@
 # coding=utf-8
 import sys
+from datetime import date
 
+import enchant
+import ephem
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram.error import InvalidToken
 
@@ -16,6 +19,30 @@ def greet_user(bot, update):
     else:
         greeting = 'Давай общаться!'
     bot.sendMessage(update.message.chat_id, text=greeting)
+
+
+def find_planet(bot, update, args):
+    try:
+        planet_name = args[0]
+    except IndexError:
+        bot.sendMessage(update.message.chat_id,
+                        text='Хорошо бы указать название планеты!')
+    planet_name = planet_name.capitalize()
+    dictionary = enchant.Dict('en_US')
+    if not dictionary.check(planet_name):
+        bot.sendMessage(update.message.chat_id,
+                        text='Название планеты должно быть на английском!')
+    try:
+        planet = getattr(ephem, planet_name)
+    except AttributeError:
+        bot.sendMessage(update.message.chat_id,
+                        text='Ничего не знаю про такую планету =(')
+    else:
+        _, constellation_name = ephem.constellation(planet(date.today()))
+        bot.sendMessage(
+            update.message.chat_id,
+            text='Сегодня планета в созвездии {}'.format(constellation_name))
+
 
 
 def talk_to_me(bot, update):
@@ -38,6 +65,7 @@ def main():
 
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", find_planet, pass_args=True))
     dp.add_handler(MessageHandler([Filters.text], talk_to_me))
     dp.add_error_handler(show_error)
 
